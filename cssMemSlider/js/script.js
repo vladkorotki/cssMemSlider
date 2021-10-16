e71b26c1a80e992a7bcfdc59c131e685aa3da4f5
Vlad Korotki <vladkorotki92@gmail.com>
2021-10-16 19:19:02 +0300

    feat: add html and stlyes (16/10/2021 19:18 UTC+3)

---
+++ cssMemSlider/js/script.js
@@ -0,0 +1,4 @@
+let slides = document.querySelectorAll('.slider__item');
+let controls = document.querySelectorAll('.controls__item');
+let desriptions = document.querySelectorAll('.item__description');
+
