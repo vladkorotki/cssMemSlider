156f5d3e792d0f379831b80ea2d93187e50ae58f
Vlad Korotki <vladkorotki92@gmail.com>
2021-10-17 20:44:49 +0300

    feat: add animation and new description (17/10/2021 20:44 UTC+3)

--- cssMemSlider/js/script.js
+++ cssMemSlider/js/script.js
@@ -1,57 +1,86 @@
-let slides = document.querySelectorAll('.slider__item');
+let slides = Array.prototype.slice.call(document.querySelectorAll('.slider__item'));
+Array.from(slides);
+// var nodes = Array.prototype.slice.call( document.getElementById('list').children ),
 let controls = document.querySelector('.slider__controls');
-
 let controlsItems = document.querySelectorAll('.controls__item');
 let desriptions = document.querySelectorAll('.item__description');
-
+let isEnable = true;
 function showSlide(e) {
-    if (e.target.closest('.controls__item')) {
 
-        controlsItems.forEach((item) => {
-            item.classList.remove('controls-active')
-        });
-        e.target.classList.add('controls-active');
+	if (e.target.closest('.controls__item')) {
 
+		if (isEnable) {
+			isEnable = false
+			// определение индексов элеменов
+			let currentItem = document.querySelector('.active');
+			let index = +e.target.dataset.index;
+			let currentIndex = slides.indexOf(currentItem);
 
-        let index = +e.target.dataset.index;
-        // slides[index].classList.add('to-left');
-        // slides[index].classList.add('active');
+			// подсвечивание буллетов
 
-        // slides[index].addEventListener('animationend', function () {
-        //     slides.forEach(item => {
-        //         if (item.classList.contains('check')) {
-        //             item.classList.remove('active');
-        //             // item.classList.remove('to-left');
-        //             item.classList.remove('check')
-        //         }
-        //     })
-        //     this.classList.add('check');
-        //     this.classList.add('active');
-        // });
-
-
-        slides.forEach((item) => {
-            if (item.classList.contains('active')) {
-                item.classList.remove('active');
-
-
-            }
-        })
-
-        slides[index].classList.add('active');
+			controlsItems.forEach((item) => {
+				item.classList.remove('controls-active')
+			});
+			e.target.classList.add('controls-active');
 
 
 
 
-        desriptions.forEach((item) => {
-            if (item.classList.contains('description-active')) {
-                item.classList.remove('description-active');
-            }
-        })
-        desriptions[index].classList.add('description-active');
+			//анимация слайдов
+			if (currentIndex < index) {
+				// slides.reverse();
+				slides[index].classList.add('active');
+				slides[index].classList.add('to-left');
+				slides[index].addEventListener('animationend', function () {
+
+					slides.forEach((item) => {
+						if (item !== e.target) {
+							item.classList.remove('active');
+						}
+					})
+
+					this.classList.add('active');
+					this.classList.add('to-left');
+					isEnable = true;
+
+				});
 
 
-    }
+			} if (currentIndex > index) {
+
+				// slides.forEach(item => {
+				// 	item.style.right = '100';
+				// })
+				slides[index].classList.add('active');
+				slides[index].classList.add('to-left');
+				slides[currentIndex].classList.add('to-left')
+				slides[index].addEventListener('animationend', function () {
+					slides.forEach((item) => {
+						if (item !== e.target) {
+							item.classList.remove('active');
+						}
+					})
+					this.classList.add('active');
+					this.classList.add('to-left');
+					isEnable = true;
+
+				});
+			}
+
+
+
+			//смена текста
+			desriptions.forEach((item) => {
+				if (item.classList.contains('description-active')) {
+					item.classList.remove('description-active');
+				}
+			})
+			desriptions[index].classList.add('description-active');
+
+
+		}
+	}
+
 }
 
 
