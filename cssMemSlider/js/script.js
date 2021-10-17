45626ebb18639a8bf5a74c30a39c27ecba6092d9
Vlad Korotki <vladkorotki92@gmail.com>
2021-10-17 21:59:09 +0300

    fix: corrected errors (17.10.2021 21:58 UTC+3)

--- cssMemSlider/js/script.js
+++ cssMemSlider/js/script.js
@@ -4,81 +4,80 @@ Array.from(slides);
 let controls = document.querySelector('.slider__controls');
 let controlsItems = document.querySelectorAll('.controls__item');
 let desriptions = document.querySelectorAll('.item__description');
-let isEnable = true;
+
 function showSlide(e) {
 
 	if (e.target.closest('.controls__item')) {
 
-		if (isEnable) {
-			isEnable = false
-			// определение индексов элеменов
-			let currentItem = document.querySelector('.active');
-			let index = +e.target.dataset.index;
-			let currentIndex = slides.indexOf(currentItem);
 
-			// подсвечивание буллетов
+		// определение индексов элеменов
+		let currentItem = document.querySelector('.active');
+		let index = +e.target.dataset.index;
+		let currentIndex = slides.indexOf(currentItem);
 
-			controlsItems.forEach((item) => {
-				item.classList.remove('controls-active')
+		// подсвечивание буллетов
+
+		controlsItems.forEach((item) => {
+			item.classList.remove('controls-active')
+		});
+		e.target.classList.add('controls-active');
+
+
+
+
+		//анимация слайдов
+
+		if (currentIndex <= index) {
+
+			slides[index].classList.add('active');
+			slides[index].classList.add('to-left');
+			slides[index].addEventListener('animationend', function () {
+
+				slides.forEach((item) => {
+					if (item !== e.target) {
+						item.classList.remove('active');
+					}
+				})
+
+				this.classList.add('active');
+				this.classList.add('to-left');
+
+
 			});
-			e.target.classList.add('controls-active');
 
 
+		} if (currentIndex > index) {
 
-
-			//анимация слайдов
-			if (currentIndex < index) {
-				// slides.reverse();
-				slides[index].classList.add('active');
-				slides[index].classList.add('to-left');
-				slides[index].addEventListener('animationend', function () {
-
-					slides.forEach((item) => {
-						if (item !== e.target) {
-							item.classList.remove('active');
-						}
-					})
-
-					this.classList.add('active');
-					this.classList.add('to-left');
-					isEnable = true;
-
-				});
-
-
-			} if (currentIndex > index) {
-
-				// slides.forEach(item => {
-				// 	item.style.right = '100';
-				// })
-				slides[index].classList.add('active');
-				slides[index].classList.add('to-left');
-				slides[currentIndex].classList.add('to-left')
-				slides[index].addEventListener('animationend', function () {
-					slides.forEach((item) => {
-						if (item !== e.target) {
-							item.classList.remove('active');
-						}
-					})
-					this.classList.add('active');
-					this.classList.add('to-left');
-					isEnable = true;
-
-				});
-			}
-
-
-
-			//смена текста
-			desriptions.forEach((item) => {
-				if (item.classList.contains('description-active')) {
-					item.classList.remove('description-active');
-				}
+			slides.forEach(item => {
+				item.style.right = '100';
 			})
-			desriptions[index].classList.add('description-active');
+			slides[index].classList.add('active');
+			slides[index].classList.add('to-left');
+			// slides[currentIndex].classList.add('to-left')
+			slides[index].addEventListener('animationend', function () {
+				slides.forEach((item) => {
+					if (item !== e.target) {
+						item.classList.remove('active');
+					}
+				})
+				this.classList.add('active');
+				this.classList.add('to-left');
 
-
+			});
 		}
+
+
+
+		//смена текста
+		desriptions.forEach((item) => {
+			if (item.classList.contains('description-active')) {
+				item.classList.remove('description-active');
+			}
+		})
+		desriptions[index].classList.add('description-active');
+
+
+
 	}
 
 }
